7c606a18bcbaa306dd23170c8167054aa0546f2d
Estrada Real Experimental <experimental@estradareal.com>
2026-01-20 18:52:06 -0300

    feat: Add environment variables support for Vercel deploy

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wtlqjbekwxpnpttiequy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bHFqYmVrd3hwbnB0dGllcXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NDA2MzcsImV4cCI6MjA4NDUxNjYzN30.VVDQKoeSxfMHQaWIVy54GhJSQnwxSkddqW1OSfUNc54';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://wtlqjbekwxpnpttiequy.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bHFqYmVrd3hwbnB0dGllcXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NDA2MzcsImV4cCI6MjA4NDUxNjYzN30.VVDQKoeSxfMHQaWIVy54GhJSQnwxSkddqW1OSfUNc54';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
